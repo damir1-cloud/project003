a0aeb4bc3ce5062ec5ad92ab3e842808ef3a2a54
damir1-cloud <66561971+damir1-cloud@users.noreply.github.com>
2020-06-09 17:38:13 +0500

    AaAa

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React from 'react';
 //import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App(props) {
   return (
-    <div>Hello World by Danyal</div>
+  <div>Hello World. My name is {props.name} and my age is {props.age}</div>
   );
 }
 
